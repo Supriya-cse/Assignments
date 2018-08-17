919a987f2ae387d98d2c34eabfe88f76d52ad0dc
supriya <supriya.muppiri@gmail.com>
2018-08-17 12:00:01 +0530

    first commit

---
+++ PrimeFactors/src/com/jetbrains/Main.java
@@ -0,0 +1,25 @@
+package com.jetbrains;
+import java.util.Scanner;
+public class Main {
+    public static void generate(int n){
+        if(n==1)
+            System.out.println();
+        else {
+            for (int i = 2; i <= n; i++) {
+                while (n % i == 0) {
+                    System.out.print(i + " ");
+                    n = n / i;
+                }
+                if (n < 1)
+                    System.out.println(n);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter input:");
+        int n=sc.nextInt();
+        generate(n);
+    }
+}
