3d86e1a8e2226271736781168e3f474498f92082
supriya <supriya.muppiri@gmail.com>
2018-08-17 10:03:16 +0530

    Fourth Commit

---
+++ RightTriangle/src/com/jetbrains/Main.java
@@ -0,0 +1,18 @@
+package com.jetbrains;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the input:");
+        int input = sc.nextInt();
+        for (int i = 0; i < input; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
