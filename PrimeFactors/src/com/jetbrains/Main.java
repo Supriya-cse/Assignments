cc98b482cc625694be12ae5a88a18f8ee102367f
supriya <supriya.muppiri@gmail.com>
2018-08-17 12:03:07 +0530

    first

--- PrimeFactors/src/com/jetbrains/Main.java
+++ PrimeFactors/src/com/jetbrains/Main.java
@@ -7,12 +7,13 @@ public class Main {
         else {
             for (int i = 2; i <= n; i++) {
                 while (n % i == 0) {
-                    System.out.print(i + " ");
+                    System.out.print(i + ",");
                     n = n / i;
                 }
                 if (n < 1)
                     System.out.println(n);
             }
+            System.out.println("\b");
         }
     }
 
